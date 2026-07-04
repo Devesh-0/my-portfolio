b1f51b7349e878ed84861774919efcbf25b667fd
Devesh Sureka <dev05sureka@gmail.com>
2026-07-04 20:13:11 +0530

    Initial commit - portfolio website

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#name");
+const formMessage = document.querySelector("#formMessage");
+const emailInput = document.querySelector("#email");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const userName = nameInput.value;
+    const userEmail = emailInput.value;
+
+    if(userName === "") {
+        formMessage.textContent = "Please enter your name before sending.";
+    } else if(userEmail ==="") {
+        formMessage.textContent = "Please enter your email before sending.";
+    } else {
+        formMessage.textContent = "Thanks, " + userName + "! Your message has been sent.";
+    }
+});
